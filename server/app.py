30eea98dda569059be722c3aa5d70898f10a03ba
Margaret <mungaimargaretn@gmail.com>
2024-07-04 09:56:53 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
 from flask import Flask, request, jsonify
+from flask_restful import Api
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -10,14 +11,13 @@ DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db'
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.json.compact = False
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = False  
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
 api = Api(app)
-
 
 @app.route("/")
 def index():
